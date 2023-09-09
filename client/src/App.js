d515cfd84ed8b00bec0bc7c14acec006a13e0391
Tean Lai <laitean19@gmail.com>
2023-09-09 18:08:23 -0400

    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,41 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import { Document, Page, pdfjs } from 'react-pdf';
+import { useDropzone } from 'react-dropzone';
+
+import './App.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function App() {
   const [file, setFile] = useState(null);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [pdfUrl, setPdfUrl] = useState(null);
   const [numPages, setNumPages] = useState(null);
 
-  // 
-  const handleFileChange = event => {
-    const selectedFile = event.target.files[0];
-    setFile(selectedFile);
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop: acceptedFiles => {
+      setFile(acceptedFiles[0]);
+    },
+    accept: 'application/pdf'
+  });
 
-    // Convert selected file to URL and set it
-    const fileUrl = URL.createObjectURL(selectedFile);
-    setPdfUrl(fileUrl);
-  };
-
-  const handleUpload = async () => {
-    const formData = new FormData();
-    formData.append('pdf', file);
-
-    try {
-      // Assume the API endpoint is 'http://localhost:5000/upload'
-      const response = await axios.post('http://localhost:5000/upload', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      });
-      console.log(response.data);
-    } catch (error) {
-      console.error('Error uploading the file:', error);
-    }
-  };
-
-  const onDocumentLoadSuccess = ({ numPages }) => {
+  function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
-  };
+  }
 
   return (
     <div className="App">
-      <div>
-        <input type="file" onChange={handleFileChange} accept=".pdf" />
-        <button onClick={handleUpload}>Upload PDF</button>
+      <div className="header">PDF Viewer</div>
+
+      <div {...getRootProps({ className: 'dropzone' })}>
+        <input {...getInputProps()} />
+        <p>Drag & drop your PDF here, or <span className="clickable">click to select one</span></p>
       </div>
 
-      <div>
-        <input
-          type="text"
-          placeholder="Search..."
-          value={searchTerm}
-          onChange={e => setSearchTerm(e.target.value)}
-        />
-      </div>
-
-      <div>
-        {pdfUrl && (
-          <Document
-            file={pdfUrl}
-            onLoadSuccess={onDocumentLoadSuccess}
-          >
-            {Array.from(new Array(numPages), (el, index) => (
-              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-            ))}
-          </Document>
-        )}
-      </div>
+      {file && <div className="pdf-container">
+        <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+          {Array.from(new Array(numPages), (el, index) => (
+            <Page key={`page_${index + 1}`} pageNumber={index + 1} width={600} renderAnnotationLayer={false} />
+          ))}
+        </Document>
+      </div>}
     </div>
   );
 }
