f8acb01165eb500471d7a97e29bbb19694991e5f
tanj85 <jt200422@gmail.com>
2023-09-09 15:49:36 -0400

    more stuff

---
+++ flask-server/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from metaphor_python import Metaphor
+
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/request', methods=['POST'])
+def make_api_request():
+    data = request.json
+    user_input = data.get('userInput')
+    metaphor = Metaphor("06c5ba21-eed3-41bc-bc33-2b964a3aa486")
+
+    print(user_input)
+
+    response = metaphor.search(
+    user_input,
+    num_results=10,
+    use_autoprompt=True,
+    )
+
+    results = response.results
+
+    return jsonify(results) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
