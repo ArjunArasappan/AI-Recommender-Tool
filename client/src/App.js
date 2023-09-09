229ce6fdbe886ff46440447f7cde8da1136e7386
tanj85 <jt200422@gmail.com>
2023-09-09 15:37:55 -0400

    updated requirements and also the thing kinda works

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [input, setInput] = useState('');
+    const [data, setData] = useState(null);
+
+    const handleSubmit = async () => {
+        try {
+            const response = await axios.post('http://localhost:5000/api/request', { userInput: input });
+            setData(response.data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
+
+    return (
+        <div>
+            <input value={input} onChange={e => setInput(e.target.value)} />
+            <button onClick={handleSubmit}>Submit</button>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+    );
 }
 
 export default App;
