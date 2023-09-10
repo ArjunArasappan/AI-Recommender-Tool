cd4de4b3d579fd8902a9b95510f94d53f6de8c9f
Tean Lai <laitean19@gmail.com>
2023-09-10 00:40:06 -0400

    Tab 3

--- client/src/Tabs.js
+++ client/src/Tabs.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Tab1 from './Tab1';
 import Tab2 from './Tab2';
+import Tab3 from './Tab3';
 
 const Tabs = () => {
     const [activeTab, setActiveTab] = useState('tab1');
@@ -10,9 +11,11 @@ const Tabs = () => {
             <div>
                 <button onClick={() => setActiveTab('tab1')}>Tab 1</button>
                 <button onClick={() => setActiveTab('tab2')}>Tab 2</button>
+                <button onClick={() => setActiveTab('tab3')}>Tab 3</button>
             </div>
             {activeTab === 'tab1' && <Tab1 />}
             {activeTab === 'tab2' && <Tab2 />}
+            {activeTab === 'tab3' && <Tab3 />}
         </div>
     );
 }
